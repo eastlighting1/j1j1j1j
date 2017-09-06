ff9b1e1867ef277ba388dce20ffd4b60630df67a
eastlighting1 <31691750+eastlighting1@users.noreply.github.com>
2017-09-06 19:20:35 +0900

    Update ProcessTask.java
    학교 코드 수정

--- app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/tool/ProcessTask.java
+++ app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/tool/ProcessTask.java
@@ -39,7 +39,7 @@ public abstract class ProcessTask extends AsyncTask<Integer, Integer, Long> {
         publishProgress(5);
 
         final String CountryCode = "ice.go.kr"; // 접속 할 교육청 도메인
-        final String schulCode = "E100001786"; // 학교 고유 코드
+        final String schulCode = "E100001707"; // 학교 고유 코드
         final String schulCrseScCode = "4"; // 학교 종류 코드 1
         final String schulKndScCode = "04"; // 학교 종류 코드 2
 
